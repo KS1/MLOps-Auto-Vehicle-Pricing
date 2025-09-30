eb2a9b7ae4c0a5ec402debc8c7cdb640181a9dab
Kartiki Saraf <3493839+KS1@users.noreply.github.com>
2025-09-29 22:36:49 -0700

    Update pipeline.py

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,1 +1,11 @@
+from azure.identity import DefaultAzureCredential
+from azure.ai.ml import MLClient
+
+credential = DefaultAzureCredential()
+ml_client = MLClient(
+    credential=credential,
+    subscription_id=os.environ["AZURE_SUBSCRIPTION_ID"],
+    resource_group_name=os.environ["AZURE_RESOURCE_GROUP"],
+    workspace_name=os.environ["AZURE_WORKSPACE_NAME"]
+)
 
